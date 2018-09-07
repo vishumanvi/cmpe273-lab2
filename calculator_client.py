fc2fcc9c096f9c23b28aee79e847c257158282a1
Vishwanath Manvi <vishu@Vishwanaths-MacBook-Pro.local>
2018-09-06 23:36:34 -0700

    Initial version of lab2-grpc for add

---
+++ calculator_client.py
@@ -0,0 +1,39 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC Calculator client."""
+
+from __future__ import print_function
+re
+import grpc
+
+import calculator_pb2
+import calculator_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = calculator_pb2_grpc.CalculatorStub(channel)
+        response = stub.add(calculator_pb2.addRequest(num1=5, num2=25))
+        f = open("output.txt", 'a')
+        f.write("\n Calculator client received: %d" %response.sum)
+        f.close()
+
+    print("\n Calculator client received: %d" %response.sum)
+
+
+if __name__ == '__main__':
+    run()
